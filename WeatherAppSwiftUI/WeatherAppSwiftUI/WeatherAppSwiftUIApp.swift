8b096363c2fa29be9109ae5ea6dae7691902f736
Kamil Grzeczkowski <kamilgrzeczkowski@gmail.com>
2021-07-30 10:49:08 +0200

    Add basic project for WeatherAppSwiftUI

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Kamil Grzeczkowski on 30/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
