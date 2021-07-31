7978c1ec5b12ad1e74c2b49f7521ecedb00c8716
Kamil Grzeczkowski <kamilgrzeczkowski@gmail.com>
2021-07-31 22:21:06 +0200

    Initialize project for next exercise

---
+++ Formatable Diary/Formatable Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Formatable Diary
+//
+//  Created by Kamil Grzeczkowski on 31/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
