726933aeefcceffedfcd96b9837409c7ec963c26
Kamil Grzeczkowski <kamilgrzeczkowski@gmail.com>
2021-08-05 15:59:00 +0200

    add Color Picker

--- Magic Drawing Board/Magic Drawing Board/ViewController.swift
+++ Magic Drawing Board/Magic Drawing Board/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var palette: UIStackView!
+    @IBOutlet var colorButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var newImageButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var loadButton: UIBarButtonItem!
+    
+    @IBOutlet weak var canvas: UIImageView!
+    
+    @IBAction func colorButtonClicked(_ sender: UIBarButtonItem) {
+        self.palette.isHidden = !self.palette.isHidden
+    }
+    @IBAction func colorPicked(_ sender: UIButton) {
+        self.colorButton.image = UIImage(named: "redColor")
+        self.colorButton.tintColor = sender.tintColor
+        self.palette.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
 }
 
